2e6eb7e7d30bde2f537b8d9f29d352f84c99b40c
Kaviya <Kaviya@DESKTOP-54QEQII>
2020-12-17 17:21:01 +0530

    added subs method in Calculator class from branch br1

--- DemoApplication/src/demo/Calculator.java
+++ DemoApplication/src/demo/Calculator.java
@@ -4,4 +4,7 @@ public class Calculator {
 	public void add(int num1,int num2) {
 		System.out.println("sum: " + (num1+num2));
 	}
+	public void subs(int num1,int num2) {
+		System.out.println("Difference: " + (num1-num2));
+	}
 }
