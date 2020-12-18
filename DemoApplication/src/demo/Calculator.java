bd40aa0220894edd423bc9dd3c4379fb1961be09
Kaviya <Kaviya@DESKTOP-54QEQII>
2020-12-18 13:42:24 +0530

    Added product method in Calculator class

--- DemoApplication/src/demo/Calculator.java
+++ DemoApplication/src/demo/Calculator.java
@@ -7,4 +7,7 @@ public class Calculator {
 	public void difference(int num1,int num2) {
 		System.out.println("Difference: " + (num1-num2));
 	}
+	public void product(int num1,int num2) {
+		System.out.println("Product: " + (num1*num2));
+	}
 }
