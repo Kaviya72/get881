bac291700a5cd8e640cb4f47cabe904b75ae9b40
Kaviya <Kaviya@DESKTOP-54QEQII>
2020-12-18 15:31:33 +0530

    removed methods

--- DemoApplication/src/demo/Calculator.java
+++ DemoApplication/src/demo/Calculator.java
@@ -4,9 +4,7 @@ public class Calculator {
 	public void add(int num1,int num2) {
 		System.out.println("sum: " + (num1+num2));
 	}
-	public void difference(int num1,int num2) {
-		System.out.println("Difference: " + (num1-num2));
-	}
+	
 	public void product(int num1,int num2) {
 		System.out.println("Product: " + (num1*num2));
 	}
